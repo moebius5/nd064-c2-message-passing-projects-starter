057cf79c7f6dde34f1aed2cbcb77154183a932fe
moebius5 <muhamed.berdimuradov@gmail.com>
2021-11-13 00:27:08 +0200

    completed locations-kafka2db container

---
+++ modules/locations-kafka2db/app/udaconnect/services.py
@@ -0,0 +1,40 @@
+import logging
+from datetime import datetime, timedelta
+from typing import Dict, List
+
+#######from app import db
+from app.udaconnect.models import Connection, Location, Person
+from app.udaconnect.schemas import LocationSchema
+from geoalchemy2.functions import ST_AsText, ST_Point
+from sqlalchemy.sql import text
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app.config import *
+
+logging.basicConfig(level=logging.WARNING)
+logger = logging.getLogger("udaconnect-api")
+
+#engine = create_engine('postgresql://ct_admin:wowimsosecure@localhost:32543/geoconnections')
+engine = create_engine(f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
+Session = sessionmaker(bind=engine)
+session = Session()
+
+class LocationService:
+    @staticmethod
+    def create(location: Dict) -> Location:
+        validation_results: Dict = LocationSchema().validate(location)
+        if validation_results:
+            logger.warning(f"Unexpected data format in payload: {validation_results}")
+            raise Exception(f"Invalid payload: {validation_results}")
+
+        new_location = Location()
+        new_location.id = location["id"]
+        new_location.person_id = location["person_id"]
+        new_location.creation_time = location["creation_time"]
+        new_location.coordinate = ST_Point(location["latitude"], location["longitude"])
+        session.add(new_location)
+        session.commit()
+
+        return new_location
+
+
---
+++ modules/locations-kafka2db/main.py
@@ -0,0 +1,36 @@
+import json
+
+import logging, sys
+from kafka import KafkaConsumer
+from app.udaconnect.services import LocationService
+
+
+KAFKA_SERVER = 'kafka:9092'
+TOPIC_NAME = 'locations'
+
+
+def main():
+    consumer = KafkaConsumer(bootstrap_servers=KAFKA_SERVER,
+                             value_deserializer=lambda m: json.loads(m.decode('utf-8')))
+    consumer.subscribe([TOPIC_NAME])
+    try:
+        for message in consumer:
+            location=message.value
+            LocationService.create(location)
+            logger.info("Location data:" + str(location) + "saved to database.")
+    except KeyboardInterrupt:
+        consumer.unsubscribe()
+        consumer.close()
+
+# Some logging stuff
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+stdouth = logging.StreamHandler(sys.stdout)
+stdouth.setLevel(logging.INFO)
+formatter = logging.Formatter('%(levelname)s:%(name)s:%(asctime)s, %(message)s', datefmt='%m/%d/%Y, %H:%M:%S')
+stdouth.setFormatter(formatter)
+logger.addHandler(stdouth)
+
+
+if __name__ == "__main__":
+    main()
